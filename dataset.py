a560b984ff12135b47367c6070d7e1c0319d1e6b
durzal1 <67489054+durzal1@users.noreply.github.com>
2023-08-04 16:39:39 -0400

    data completely done. starting models

---
+++ dataset.py
@@ -0,0 +1,105 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import pandas as pd
+from torchtext.data.utils import get_tokenizer
+from transformers import BertTokenizer
+from torchtext.vocab import build_vocab_from_iterator
+import torch.nn.functional as F
+
+class ImageCaptionDataset(Dataset):
+    def __init__(self, image_paths, caption_file, caption_length, transform=None):
+        self.image_path = image_paths
+        self.caption_file = caption_file
+        self.transform = transform
+        self.caption_length = caption_length
+
+        self.tokenizer = get_tokenizer('basic_english')
+
+
+        self.SetUpCaptions()
+
+    def SetUpCaptions(self):
+        captions = pd.read_csv("results.csv", sep="|")
+
+        num_rows, num_columns = captions.shape
+
+        # All the data we need
+        self.image_names = []
+        self.comment_numbers = []
+        self.comments = []
+
+        # For the vocab
+        data = []
+
+        # Get Data
+        for ind in range(0,num_rows):
+            # if ind == 19999:
+            #     continue
+            # if ind == 1000:
+            #     break
+
+            # only get every 5th image
+            if ind % 5 == 0:
+                image_name = captions.loc[ind,"image_name"]
+                self.image_names.append(image_name)
+
+            comment_number = captions.loc[ind," comment_number"]
+            comment = captions.loc[ind," comment"]
+
+            # Broken index for some reason
+            if ind == 19999:
+                comment = "a dog runs across the grass"
+
+            self.comment_numbers.append(comment_number)
+            self.comments.append(comment)
+
+            data.append(self.tokenizer(comment))
+
+        self.vocab = build_vocab_from_iterator(data, specials= ["<PAD>", "<UNK>", "<SOS>", "<EOS>"], min_freq=5)
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, index):
+
+        image = self.image_names[index]
+
+        caption_tensor = torch.zeros((5, self.caption_length), dtype=torch.int32)
+
+
+        for i in range(5):
+            caption_index = index * 5 + i
+            caption = self.comments[caption_index]
+
+            # Tokenize the text
+            tokens = self.tokenizer(caption)
+
+            # Create the numerical representation
+            numerical_representation = torch.tensor([self.vocab[token] for token in tokens])
+
+            numerical_representation = numerical_representation.squeeze(0)
+
+            # Compute the padding amount
+            padding_amount = max(0, self.caption_length - numerical_representation.size(0))
+
+            # Define the padding tuple for the specified dimension
+            padding = [0] * (2 * len(numerical_representation.size()))
+            padding[0 * 2 + 1] = padding_amount
+
+            # Apply padding to the tensor
+            padded_representation = F.pad(numerical_representation, padding, value=0)
+
+
+            caption_tensor[i] = padded_representation
+
+        specific_image_path = os.path.join(self.image_path, image)
+        image = Image.open(specific_image_path).convert("RGB")
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, caption_tensor
--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from torchvision import transforms
 from torchvision.datasets import CocoCaptions
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer
-
+from dataset import *
+import torch.nn as nn
 
 # Directory for images and captions (From COCO dataset)
-images_dir = "train2014/"
-captions_file = "captions_train2014.json"
+images_dir = "flickr30k_images/"
+captions_file = "results.csv"
 
 # Constants
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
 BATCH_SIZE = 32
-CAPTION_LENGTH = 10
+CAPTION_LENGTH = 25
 
 # Image preprocessing and normalization
 image_transform = transforms.Compose([
@@ -22,41 +23,44 @@ image_transform = transforms.Compose([
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
-# Load the MS COCO dataset
-dataset = CocoCaptions(root=images_dir, annFile=captions_file, transform=image_transform)
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-# Prepare the DataLoader with collate_fn for padding
-def collate_fn(batch):
-    images, captions = zip(*batch)
-
-    # Not sure why I need this, but it breaks without it.
-    # All images should already be the same size
-    images_padded = torch.nn.utils.rnn.pad_sequence(images, batch_first=True)
-
-    ##TODO FIGURE OUT
-    # Convert each batch of captions to tensors
-    captions_tensors = []
-    for caption_batch in captions:
-        caption_tensors = []
-        for caption in caption_batch:
-            # Assuming you have a tokenizer function to tokenize the sentences (e.g., spaCy or NLTK)
-            # You should replace this tokenizer function with the one you use in your specific case.
-            tokens = tokenizer(caption)
-            caption_tensors.append(torch.tensor(tokens))
-        captions_tensors.append(torch.stack(caption_tensors))
-
-    # Make them all the same size
-    captions_padded = torch.nn.utils.rnn.pad_sequence(captions_tensors, batch_first=True)
-
-    print('f')
-    # return images_padded, captions_tokenized
+# Load the Flickr30k dataset
+dataset = ImageCaptionDataset(images_dir, captions_file, CAPTION_LENGTH, transform=image_transform)
 
 # Prepare the DataLoader
-data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
+data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+dataset.__getitem__(1)
 
-for batch_idx, (x, y) in enumerate(data_loader):
-    print("Images shape:", x.shape)
-    print("Captions:", y)
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers=1):
+        super(Decoder, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
+
+    def forward(self, features, captions):
+        # features: Image features (output of the encoder)
+        # captions: Ground truth captions for teacher forcing (batch_size, caption_length)
+
+        # Remove the last token from captions (input to LSTM) to get the input sequence
+        input_captions = captions[:, :-1]
+
+        # Embed the input captions
+        embedded_captions = self.embedding(input_captions)
+
+        # Concatenate image features with embedded captions
+        inputs = torch.cat((features.unsqueeze(1), embedded_captions), dim=1)
+
+        # Pass the inputs through the LSTM
+        lstm_outputs, _ = self.lstm(inputs)
+
+        # Final output prediction at each time step
+        outputs = self.fc(lstm_outputs)
+
+        return outputs
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
